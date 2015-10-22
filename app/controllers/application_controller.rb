cbb9dcd69488e4cfd5585a9df815ef1bd55a9e7c
cybernick <cyberbugoff@gmail.com>
2015-10-22 16:02:31 +0300

    Adds authenticable module for managing authentication methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :null_session
+  include Authenticable
   protected
 
   def configure_permitted_parameters
